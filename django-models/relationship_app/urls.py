3d43b7347d338a7c936fbcaf3b83a63b79ede388
Masri <masrialemu404@gmail.com>
2024-08-15 15:46:56 +0300

    v1.0.9

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,5 +4,9 @@ from .views import list_books
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
+    path('login/', list_books, name='login'),
+    path('logout/', list_books, name='logout'),
+    path('register/', list_books, name='register'),
     path('library/<pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,11 +3,45 @@ from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from .models import Book 
 from .models import Library
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 def list_books(request):
    
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')  # Redirect to a view after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')  # Redirect to a view after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+
 
 class LibraryDetailView(DetailView):
  
