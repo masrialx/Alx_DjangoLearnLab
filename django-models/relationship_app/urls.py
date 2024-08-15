6e775806c97ca49bc1b6454d1626dec8cc7ee9bb
Masri <masrialemu404@gmail.com>
2024-08-15 16:25:55 +0300

    v1.1.9

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.edit_book, name='edit_book'),
 
-    # Authentication views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
@@ -17,7 +17,7 @@ urlpatterns = [
     # Library detail view
     path('library/<pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Role-based views
+    
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
