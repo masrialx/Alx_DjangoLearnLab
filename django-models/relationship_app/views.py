28d3fdd29a31b12ce84ddfab64a7060fa1cbf371
Masri <masrialemu404@gmail.com>
2024-08-15 15:29:15 +0300

    v1.0.2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,4 +8,4 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
