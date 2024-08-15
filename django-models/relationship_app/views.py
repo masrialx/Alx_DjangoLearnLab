9fb15bf058c8a3e68dffcb23a10bd8937482ae1e
Masri <masrialemu404@gmail.com>
2024-08-15 16:03:19 +0300

    v1.1.4

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.models import User
-from django.http import HttpResponseForbidden
+from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+from .models import Book 
+from .models import Library
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+def list_books(request):
+   
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Helper functions to check roles
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
 
-def is_member(user):
-    return user.userprofile.role == 'Member'
 
-# Views for each role
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+class LibraryDetailView(DetailView):
+ 
+    model = Library
+  
+    template_name = 'relationship_app/library_detail.html'
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
+    context_object_name = 'library'
 
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+    def get_context_data(self, **kwargs):
+     
+        context = super().get_context_data(**kwargs)
+       
+        context['books'] = self.object.books.all() 
+        return context
