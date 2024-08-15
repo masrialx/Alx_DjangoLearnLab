9724b4972aacc09de75857bb8561b27fb1f1f8be
Masri <masrialemu404@gmail.com>
2024-08-15 13:56:55 +0300

    v0.6.4

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', views.user_register, name='register'),  # Ensure this line is present
+    path('login/', views.user_login, name='login'),  # Use your custom login view
+    path('logout/', views.user_logout, name='logout'),  # Use your custom logout view
+    path('register/', views.user_register, name='register'),  # Use your custom register view
 ]
