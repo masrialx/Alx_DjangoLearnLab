bc0c24f444097dbc6979091a8831651b556c95d7
Masri <masrialemu404@gmail.com>
2024-08-15 15:26:23 +0300

    v0.1.0

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,6 @@
-# relationship_app/views.py
-
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Book, Library
-from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
@@ -11,9 +9,3 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
-    context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = Book.objects.all()  # Add this line to include all books in the context
-        return context
