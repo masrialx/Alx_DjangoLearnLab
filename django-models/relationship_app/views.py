db1be59e3dec61fc2baf76c30a3559891bfeb556
Masri <masrialemu404@gmail.com>
2024-08-15 13:58:57 +0300

    v0.6.6

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,24 +1,5 @@
-from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('list_books')  # Redirect to a view after login
-        else:
-            return render(request, 'login.html', {'error': 'Invalid credentials'})
-    return render(request, 'login.html')
-
-def user_logout(request):
-    logout(request)
-    return render(request, 'logout.html')
 
 def user_register(request):
     if request.method == 'POST':
