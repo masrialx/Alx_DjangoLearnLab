ea43ad04ae22d3383df075757cff552284da965b
Masri <masrialemu404@gmail.com>
2024-08-15 15:30:48 +0300

    v1.0.2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,7 @@
+# relationship_app/views.py
+
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView
 from .models import Book, Library
 
 def list_books(request):
@@ -9,3 +11,10 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        # Add the books associated with this library to the context
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Assuming a reverse relation from Library to Book
+        return context
