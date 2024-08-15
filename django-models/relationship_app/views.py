4b07aec48144f23b96b367535f4de61c89dbf61d
Masri <masrialemu404@gmail.com>
2024-08-15 14:54:18 +0300

    v0.8.7

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,8 +5,11 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
 
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'list_books.html.html'
+    template_name = 'library_detail.html'  # Ensure this matches your actual template
     context_object_name = 'library'
