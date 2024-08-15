3e35611e02024c95abfc39af5cd4765b8a9d2e09
Masri <masrialemu404@gmail.com>
2024-08-15 16:12:46 +0300

    v1.1.6

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
     path('library/<pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    
+    # Role-Based Views
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,31 @@ from .models import Book, Library
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseForbidden
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 def list_books(request):
     books = Book.objects.all()
