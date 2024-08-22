906ad527c896eabfbc17e006fa492ae637030b5d
Masri <masrialemu404@gmail.com>
2024-08-22 15:37:06 +0300

    v0.0.6

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,8 +1,9 @@
+# bookshelf/admin.py
 from django.contrib import admin
 from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year',)
-    search_fields = ('title', 'author')
+    list_display = ('title', 'author', 'publication_year')  # Ensure this matches your model
+    list_filter = ('publication_year',)  # Ensure this matches your model
+    search_fields = ('title', 'author')  # Ensure this matches your model
--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,9 +1,10 @@
+# bookshelf/models.py
 from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    publication_year = models.IntegerField()  # Changed from published_date to publication_year
 
     def __str__(self):
         return f"{self.title} by {self.author}"
