ea9ad0cff5bc19fabe9dd8c937bb81ec8899080c
Masri <masrialemu404@gmail.com>
2024-08-22 16:00:08 +0300

    v0..0.8

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book, Library, Librarian
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year',)
-    search_fields = ('title', 'author')
+    list_display = ('title', 'author', 'published_date')
+    list_filter = ('author',)
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    filter_horizontal = ('books',)
+
+@admin.register(Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ('name', 'library')
