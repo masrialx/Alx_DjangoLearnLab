eb283a331c84663a26b4f20c8eb0ca4319fcd7d2
Masri <masrialemu404@gmail.com>
2024-08-22 14:10:31 +0300

    v0.0.4

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -4,6 +4,10 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    name = models.CharField(max_length=100)
 
     def __str__(self):
+        return self.name
+    def __str__(self):
         return f"{self.title} by {self.author}"
+
