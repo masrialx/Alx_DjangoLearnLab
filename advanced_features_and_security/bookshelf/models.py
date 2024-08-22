2ec7fd6d2a08430a3f2e19a0c9d498bae468f03b
Masri <masrialemu404@gmail.com>
2024-08-22 16:23:56 +0300

    v0.1.0

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,13 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
-from django.conf import settings
-
-class SomeModel(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        return self.username
