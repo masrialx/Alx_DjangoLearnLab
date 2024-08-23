169bfb31329a97b97245c52dc364fa697d55296d
Masri <masrialemu404@gmail.com>
2024-08-23 14:20:44 +0300

    v0.2.7

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,9 +1,16 @@
 # LibraryProject/bookshelf/forms.py
 
 from django import forms
-from .models import Book
+from .models import Book  # Import your model if needed
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date', 'isbn']
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200, label='Book Title')
+    author = forms.CharField(max_length=100, label='Author')
+    publication_year = forms.IntegerField(label='Publication Year')
+    # You can add more fields as needed
+
+    def clean_publication_year(self):
+        year = self.cleaned_data.get('publication_year')
+        if year < 0:
+            raise forms.ValidationError("Publication year cannot be negative.")
+        return year
