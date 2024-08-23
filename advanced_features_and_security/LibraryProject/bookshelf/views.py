d0d69cbc2c9cd2315bc3bb86da590f7e4c14d904
Masri <masrialemu404@gmail.com>
2024-08-23 14:22:12 +0300

    v0.2.8

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render
-from .models import Book
+from .forms import ExampleForm, BookForm  # Ensure ExampleForm is imported
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_detail(request, pk):
@@ -20,25 +18,14 @@ def book_edit(request, pk):
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
-# views.py
-
-from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required
-from .models import Book
-from .forms import BookForm
-
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)  # Use ExampleForm
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/form_example.html', {'form': form})
