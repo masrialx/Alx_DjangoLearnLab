d4b20db45e53f6bcc1d6784060d6fdbeaeee1874
Masri <masrialemu404@gmail.com>
2024-08-23 13:53:09 +0300

    v0.2.0

--- advanced_features_and_security/LibraryProject/urls.py
+++
@@ -1,9 +0,0 @@
-# LibraryProject/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship/', include('relationship_app.urls')),
-]
