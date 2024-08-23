8403d0d7f7a8b677abd1b786e8df182f11c3367c
Masri <masrialemu404@gmail.com>
2024-08-23 14:05:03 +0300

    v0.2.2

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -6,3 +6,26 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year',)
     search_fields = ('title', 'author')
+from django.contrib import admin
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year',)
+    search_fields = ('title', 'author')
+
+# CustomUserAdmin class to customize the admin interface for CustomUser
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# Registering CustomUser with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
