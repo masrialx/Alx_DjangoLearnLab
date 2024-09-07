61e458ec306337a3170c626363a431d65a8e998f
Masri <masrialemu404@gmail.com>
2024-09-07 22:32:34 +0300

    v0.1.9

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -32,3 +32,20 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]  # Restrict to authenticated users
+
+
+
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # Default ordering
