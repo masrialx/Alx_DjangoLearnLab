88b70c7157075a5c7b90d046646785970ed0cd7a
Masri <masrialemu404@gmail.com>
2024-09-07 22:36:03 +0300

    v1.0.1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # List all books
+from rest_framework.filters import SearchFilter
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -49,3 +50,5 @@ class BookListView(generics.ListAPIView):
     filterset_fields = ['title', 'author__name', 'publication_year']
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']  # Default ordering
+
+
