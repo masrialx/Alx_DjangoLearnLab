3f759ec6e69fcff73cd5f6ee92c68971f6d8e88a
Masri <masrialemu404@gmail.com>
2024-09-12 17:44:33 +0300

    v0.0.4

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile_view, name="profile"),
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
