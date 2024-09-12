4421fdf2434b06523c53aec65a16d377dc03af33
Masri <masrialemu404@gmail.com>
2024-09-12 17:59:41 +0300

    v0.0.8

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
-    path('comment/<int:pk>/edit/', views.edit_comment, name='edit_comment'),
+    path('comment/<int:pk>/update/', views.edit_comment, name='edit_comment'),
+    path('post/<int:pk>/comments/new/', views.edit_comment, name='edit_comment'),
     path('comment/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
 ]
