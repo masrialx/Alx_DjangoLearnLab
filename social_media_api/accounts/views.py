861f65360c9c0d5610e32eb173ae3a0fda2e4b55
Masri <masrialemu404@gmail.com>
2024-09-17 11:32:16 +0300

    v0.1.7

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -12,7 +12,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import CustomUser
 
-
+ss=generics.GenericAPIView, 
+sz=CustomUser.objects.all()
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
